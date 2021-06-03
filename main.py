d2c7b6dadea98f40a4915c2f825d1c93b9869273
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2021-06-03 13:26:30 +0100

    debug

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ while True:
         # If connection made in AB
         if io.switch_is_on(io.AB) and switched == False:
             io.LED_on()
+            print("LED ON")
             switched = True
         elif io.switch_is_off(io.AB) and switched == True:
             io.LED_off()
+            print("LED OFF")
             switched = False
     
 
